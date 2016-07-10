6441a3ef3b35750402e040266b2a6b12af9ec793
alshell7 <alshell@git>
2016-07-10 20:43:37 +0530

    Added source of the app
    Source + Executable

---
+++ LCD_CustomCharGenerator/LCD_CustomCharGenerator/MainForm.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LCDPanel = New System.Windows.Forms.Panel()
+        Me.OutputBox = New System.Windows.Forms.TextBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.CopyButton = New System.Windows.Forms.Button()
+        Me.ArduinoCodeBox = New System.Windows.Forms.TextBox()
+        Me.GenCodeButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CharNameTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SelectAllButton = New System.Windows.Forms.Button()
+        Me.InvertButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LCDPanel
+        '
+        Me.LCDPanel.BackColor = System.Drawing.Color.GreenYellow
+        Me.LCDPanel.Location = New System.Drawing.Point(13, 110)
+        Me.LCDPanel.Name = "LCDPanel"
+        Me.LCDPanel.Size = New System.Drawing.Size(110, 165)
+        Me.LCDPanel.TabIndex = 42
+        '
+        'OutputBox
+        '
+        Me.OutputBox.BackColor = System.Drawing.Color.White
+        Me.OutputBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.OutputBox.Font = New System.Drawing.Font("Calibri", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.OutputBox.ForeColor = System.Drawing.Color.Black
+        Me.OutputBox.Location = New System.Drawing.Point(155, 110)
+        Me.OutputBox.Multiline = True
+        Me.OutputBox.Name = "OutputBox"
+        Me.OutputBox.ReadOnly = True
+        Me.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.OutputBox.Size = New System.Drawing.Size(159, 183)
+        Me.OutputBox.TabIndex = 43
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(13, 281)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(110, 23)
+        Me.ClearButton.TabIndex = 44
+        Me.ClearButton.Text = "Clear Pixels"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'CopyButton
+        '
+        Me.CopyButton.Location = New System.Drawing.Point(155, 308)
+        Me.CopyButton.Name = "CopyButton"
+        Me.CopyButton.Size = New System.Drawing.Size(159, 23)
+        Me.CopyButton.TabIndex = 45
+        Me.CopyButton.Text = "Copy"
+        Me.CopyButton.UseVisualStyleBackColor = True
+        '
+        'ArduinoCodeBox
+        '
+        Me.ArduinoCodeBox.BackColor = System.Drawing.Color.White
+        Me.ArduinoCodeBox.Font = New System.Drawing.Font("Calibri", 10.0!)
+        Me.ArduinoCodeBox.ForeColor = System.Drawing.Color.Black
+        Me.ArduinoCodeBox.Location = New System.Drawing.Point(355, 23)
+        Me.ArduinoCodeBox.Multiline = True
+        Me.ArduinoCodeBox.Name = "ArduinoCodeBox"
+        Me.ArduinoCodeBox.ReadOnly = True
+        Me.ArduinoCodeBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.ArduinoCodeBox.Size = New System.Drawing.Size(439, 337)
+        Me.ArduinoCodeBox.TabIndex = 46
+        Me.ArduinoCodeBox.Text = "//Your arduino code will appear here"
+        '
+        'GenCodeButton
+        '
+        Me.GenCodeButton.Location = New System.Drawing.Point(155, 337)
+        Me.GenCodeButton.Name = "GenCodeButton"
+        Me.GenCodeButton.Size = New System.Drawing.Size(159, 23)
+        Me.GenCodeButton.TabIndex = 47
+        Me.GenCodeButton.Text = "Generate Arduino Code"
+        Me.GenCodeButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 13)
+        Me.Label1.TabIndex = 48
+        Me.Label1.Text = "Enter your character name :"
+        '
+        'CharNameTextBox
+        '
+        Me.CharNameTextBox.Location = New System.Drawing.Point(157, 70)
+        Me.CharNameTextBox.MaxLength = 10
+        Me.CharNameTextBox.Name = "CharNameTextBox"
+        Me.CharNameTextBox.Size = New System.Drawing.Size(159, 20)
+        Me.CharNameTextBox.TabIndex = 49
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(61, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(184, 39)
+        Me.Label2.TabIndex = 50
+        Me.Label2.Text = "(HD44780)"
+        '
+        'SelectAllButton
+        '
+        Me.SelectAllButton.Location = New System.Drawing.Point(15, 310)
+        Me.SelectAllButton.Name = "SelectAllButton"
+        Me.SelectAllButton.Size = New System.Drawing.Size(108, 23)
+        Me.SelectAllButton.TabIndex = 51
+        Me.SelectAllButton.Text = "Select All"
+        Me.SelectAllButton.UseVisualStyleBackColor = True
+        '
+        'InvertButton
+        '
+        Me.InvertButton.Location = New System.Drawing.Point(15, 339)
+        Me.InvertButton.Name = "InvertButton"
+        Me.InvertButton.Size = New System.Drawing.Size(108, 23)
+        Me.InvertButton.TabIndex = 52
+        Me.InvertButton.Text = "Invert"
+        Me.InvertButton.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(806, 376)
+        Me.Controls.Add(Me.InvertButton)
+        Me.Controls.Add(Me.SelectAllButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CharNameTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GenCodeButton)
+        Me.Controls.Add(Me.ArduinoCodeBox)
+        Me.Controls.Add(Me.CopyButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.OutputBox)
+        Me.Controls.Add(Me.LCDPanel)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LCD Custom Character Generator :)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LCDPanel As System.Windows.Forms.Panel
+    Friend WithEvents OutputBox As System.Windows.Forms.TextBox
+    Friend WithEvents ClearButton As System.Windows.Forms.Button
+    Friend WithEvents CopyButton As System.Windows.Forms.Button
+    Friend WithEvents ArduinoCodeBox As System.Windows.Forms.TextBox
+    Friend WithEvents GenCodeButton As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CharNameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents SelectAllButton As System.Windows.Forms.Button
+    Friend WithEvents InvertButton As System.Windows.Forms.Button
+
+End Class
---
+++ LCD_CustomCharGenerator/LCD_CustomCharGenerator/MainForm.vb
@@ -0,0 +1,218 @@
+﻿Public Class MainForm
+
+#Region "My Methods"
+
+    Private Sub SetUpLCDpixels()
+
+        Dim LocationX As Integer = 10
+        Dim LocationY As Integer = 10
+        For Row As Integer = 0 To 7 Step 1
+            For Column As Integer = 0 To 4 Step 1
+                Dim PixelBox As New CheckBox()
+                With PixelBox
+                    .Appearance = System.Windows.Forms.Appearance.Button
+                    .AutoSize = True
+                    .BackColor = System.Drawing.Color.White
+                    .FlatAppearance.BorderColor = System.Drawing.Color.White
+                    .FlatAppearance.BorderSize = 0
+                    .FlatAppearance.CheckedBackColor = System.Drawing.Color.Black
+                    .FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+                    .FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow
+                    .FlatStyle = System.Windows.Forms.FlatStyle.Flat
+                    .Location = New System.Drawing.Point(LocationX, LocationY)
+                    .Name = "Pixel_" & Row & Column
+                    .Text = ""
+                    .Size = New System.Drawing.Size(6, 6)
+                    .TabIndex = 0
+                    .Tag = Row & Column
+                    .UseCompatibleTextRendering = True
+                    .UseVisualStyleBackColor = False
+                End With
+                LCDPanel.Controls.Add(PixelBox)
+                AddHandler PixelBox.Click, AddressOf GenerateOutput
+                AddHandler PixelBox.CheckedChanged, AddressOf GenerateOutput
+                LocationX = LocationX + 20
+            Next
+            LocationX = 10
+            LocationY = LocationY + 20
+        Next
+
+    End Sub
+
+    Private Sub ClearPixels()
+        For Each Box In LCDPanel.Controls
+            If TypeOf Box Is CheckBox Then
+                DirectCast(Box, CheckBox).Checked = False
+            End If
+        Next
+        OutputBox.Clear()
+        ArduinoCodeBox.Clear()
+    End Sub
+    Private Sub SelectAll()
+        For Each Box In LCDPanel.Controls
+            If TypeOf Box Is CheckBox Then
+                DirectCast(Box, CheckBox).Checked = True
+            End If
+        Next
+        GenerateOutput()
+    End Sub
+    Private Sub Invert()
+        For Each Box In LCDPanel.Controls
+            If TypeOf Box Is CheckBox Then
+                Dim PixelBox As CheckBox = DirectCast(Box, CheckBox)
+                If (PixelBox.Checked) Then
+                    PixelBox.Checked = False
+                ElseIf Not PixelBox.Checked Then
+                    PixelBox.Checked = True
+                End If
+            End If
+        Next
+        GenerateOutput()
+    End Sub
+    Private Sub GenerateOutput()
+        OutputBox.Clear()
+
+        Dim RowsLCD(7) As Integer
+        Dim ColumnsLCD(4) As Integer
+
+        If String.IsNullOrEmpty(CharNameTextBox.Text) Then
+            OutputBox.AppendText("byte myCharacter[8] = {" & vbCrLf)
+        Else
+            OutputBox.AppendText("byte " & CharNameTextBox.Text.Replace(" ", "") & "[8] = {" & vbCrLf)
+        End If
+
+        For Each Box In LCDPanel.Controls
+            If TypeOf Box Is CheckBox Then
+                Dim Pixel As CheckBox = CType(Box, CheckBox)
+                Dim PixelRow As Integer = Integer.Parse(CType(Pixel.Tag, String).ElementAt(0))
+                Dim PixelColumn As Integer = Integer.Parse(CType(Pixel.Tag, String).ElementAt(1))
+                'RichTextBox1.AppendText(Pixel.Name & " : " & PixelRow & PixelColumn & " " & (IIf(Pixel.Checked, "Checked", "Unchecked")) & vbCrLf)
+                If Pixel.CheckState = CheckState.Checked Then
+                    RowsLCD(PixelRow) = 1
+                    ColumnsLCD(PixelColumn) = 1
+                ElseIf Pixel.CheckState = CheckState.Unchecked Then
+                    RowsLCD(PixelRow) = 0
+                    ColumnsLCD(PixelColumn) = 0
+                End If
+                If PixelColumn = 0 Then
+                    OutputBox.AppendText(vbTab & "0b")
+                End If
+                If PixelColumn = 4 Then
+                    'RichTextBox1.AppendText(RowsLCD(PixelRow) & ColumnsLCD(PixelColumn) & " ")
+                    If Not PixelRow = 7 Then
+                        OutputBox.AppendText(RowsLCD(PixelRow) & ",")
+                    Else
+                        OutputBox.AppendText(RowsLCD(PixelRow))
+                    End If
+                    OutputBox.AppendText(vbCrLf)
+                Else
+                    'RichTextBox1.AppendText(RowsLCD(PixelRow) & ColumnsLCD(PixelColumn) & " ")
+                    OutputBox.AppendText(RowsLCD(PixelRow))
+                End If
+                'RichTextBox1.AppendText(RowsLCD(PixelRow) & ColumnsLCD(PixelColumn) & " ")
+                'Dim r As Integer = Integer.Parse(PixelData)
+                'Dim r As Char = PixelData.ElementAt(0)
+            End If
+        Next
+        OutputBox.AppendText(vbTab & "};")
+
+        'Threading.Thread.Sleep(1000)
+        'For Each bit In RowsLCD
+        '    For Each nextbit In ColumnsLCD
+        '        RichTextBox1.AppendText(nextbit & " ")
+        '    Next
+        '    '             RichTextBox1.AppendText(vbCrLf & bit & " ")
+        '    RichTextBox1.AppendText(vbCrLf)
+        'Next
+
+
+        '    If TypeOf ctrl Is CheckBox Then
+        '        Dim x As CheckBox = DirectCast(ctrl, CheckBox)
+
+        '        Dim rc As String = CType(x.Tag, String)
+
+        '        RichTextBox1.AppendText(x.Name & " : " & rc & vbCrLf)
+        '    End If
+    End Sub
+
+    Private Sub GenerateArduinoCode()
+        ArduinoCodeBox.Clear()
+        With ArduinoCodeBox
+            .AppendText("#include <LiquidCrystal.h>" & vbCrLf)
+            .AppendText("//Your respective LCD to Arduino pin numbers" & vbCrLf)
+            .AppendText("LiquidCrystal lcd(12, 11, 5, 4, 3, 2);" & vbCrLf)
+            .AppendText(OutputBox.Text & vbCrLf)
+            .AppendText("void setup()" & vbCrLf & "{" & vbCrLf)
+            If String.IsNullOrEmpty(CharNameTextBox.Text) Then
+                .AppendText(vbTab & "lcd.createChar(0," & "myCharacter" & ");" & vbCrLf)
+            Else
+                .AppendText(vbTab & "lcd.createChar(0," & CharNameTextBox.Text & ");" & vbCrLf)
+            End If
+            .AppendText(vbTab & "lcd.begin(16, 2);" & vbCrLf)
+            .AppendText(vbTab & "lcd.write((uint8_t) 0);" & vbCrLf)
+            .AppendText("}" & vbCrLf)
+            .AppendText("void loop()" & vbCrLf & "{" & vbCrLf & "}")
+        End With
+    End Sub
+
+    'Public Sub DisplayOutput()
+    '    For row As Integer = 0 To RowsLCD.Length - 1 Step 1
+    '        For col As Integer = 0 To ColumnsLCD.Length - 1 Step 1
+    '            Dim CurrentPixel As Integer = 0
+    '            RichTextBox1.AppendText(RowsLCD(row) & ColumnsLCD(col) & " ")
+    '            'If (RowsLCD(row) = 1 And ColumnsLCD(col) = 1) Then
+    '            '    CurrentPixel = 1
+    '            '    RichTextBox1.AppendText(CurrentPixel & " ")
+    '            'Else
+    '            '    CurrentPixel = 0
+    '            '    RichTextBox1.AppendText(CurrentPixel & " ")
+    '            'End If
+    '        Next
+    '        RichTextBox1.AppendText(vbCrLf)
+    '    Next
+    'End Sub
+
+#End Region
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetUpLCDpixels()
+    End Sub
+
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ClearPixels()
+    End Sub
+
+
+    Private Sub GenCodeButton_Click(sender As Object, e As EventArgs) Handles GenCodeButton.Click
+        GenerateArduinoCode()
+    End Sub
+
+    Private Sub CharNameTextBox_TextChanged(sender As Object, e As EventArgs) Handles CharNameTextBox.TextChanged
+        CharNameTextBox.Text.Replace(" ", "")
+        If Not String.IsNullOrEmpty(CharNameTextBox.Text) Then
+            GenerateOutput()
+        Else
+            OutputBox.Clear()
+        End If
+    End Sub
+
+    Private Sub CopyButton_Click(sender As Object, e As EventArgs) Handles CopyButton.Click
+        If String.IsNullOrEmpty(OutputBox.Text) Then
+            MsgBox("No pixels were selected for code the to generate", MsgBoxStyle.Exclamation)
+        Else
+            My.Computer.Clipboard.SetText(OutputBox.Text)
+        End If
+    End Sub    
+
+    Private Sub SelectAllButton_Click(sender As Object, e As EventArgs) Handles SelectAllButton.Click
+        SelectAll()
+    End Sub
+
+    Private Sub InvertButton_Click(sender As Object, e As EventArgs) Handles InvertButton.Click
+        Invert()
+    End Sub
+    Private Sub MainForm_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        MsgBox("Developed by alshell7", MsgBoxStyle.Information)
+    End Sub
+End Class
